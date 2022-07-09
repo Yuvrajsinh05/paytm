f8935baad20fb67f1ed60e9cf87d4a8b95e7e212
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2022-07-09 15:39:56 +0530

    push

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.custo
     params['CUST_ID'] = paymentDetails.customerId;
     params['TXN_AMOUNT'] = paymentDetails.amount;
     /* where is app is hosted (heroku url)*/
-    params['CALLBACK_URL'] = 'https://paugate.herokuapp.com/callback';
+    params['CALLBACK_URL'] = 'https://paytmkro.herokuapp.com/callback';
     params['EMAIL'] = paymentDetails.customerEmail;
     params['MOBILE_NO'] = paymentDetails.customerPhone;
   
